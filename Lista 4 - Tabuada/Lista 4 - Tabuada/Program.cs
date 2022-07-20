aaf0c3b3b195e0bc011e12bed4e6b8a1cba378f1
Aloe Heros <heros.dasilva@gmail.com>
2022-07-20 13:02:36 -0300

    Lista 4 C#

---
+++ Lista 4 - Tabuada/Lista 4 - Tabuada/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lista_4___Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, resultado;
+
+            Console.WriteLine("Olá! Digite um número para construir uma tabuada: ");
+            numero = Convert.ToInt16(Console.ReadLine());
+
+            for (int x = 0; x <= 10; x++)
+            {
+                resultado = numero * x;
+                Console.WriteLine(numero + " X " + x + " = " + resultado);
+            }
+
+            Console.ReadLine();
+                    
+        }
+    }
+}
